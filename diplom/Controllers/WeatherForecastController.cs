ea42f759c562c1dcac65c8269f87344578b7e034
youremywhore <95357313+youremywhore@users.noreply.github.com>
2022-10-19 10:11:07 +0300

    1 полностью сделана

--- diplom/Controllers/WeatherForecastController.cs
+++ diplom/Controllers/WeatherForecastController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Contracts;
 
 namespace diplom.Controllers
 {
@@ -11,29 +12,20 @@ namespace diplom.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        private ILoggerManager _logger;
+        public WeatherForecastController(ILoggerManager logger)
         {
             _logger = logger;
         }
-
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<string> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            _logger.LogInfo("Вот информационное сообщение от нашего контроллера значений.");
+            _logger.LogDebug("Вот отладочное сообщение от нашего контроллера значений.");
+            _logger.LogWarn("Вот сообщение предупреждения от нашего контроллера значений.");
+            _logger.LogError("Вот сообщение об ошибке от нашего контроллера значений.");
+            return new string[] { "value1", "value2" };
         }
+        
     }
 }

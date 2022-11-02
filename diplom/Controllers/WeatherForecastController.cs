b01c7cf70ee09c7f6b31ab8501f3865bdf284eb2
youremywhore <95357313+youremywhore@users.noreply.github.com>
2022-11-02 10:25:06 +0300

    auto mapper установил и фикс проблема в второй лабе

--- diplom/Controllers/WeatherForecastController.cs
+++ diplom/Controllers/WeatherForecastController.cs
@@ -12,21 +12,21 @@ namespace diplom.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private ILoggerManager _logger;
-        public WeatherForecastController(ILoggerManager logger)
+        private readonly IRepositoryManager _repository;
+        public WeatherForecastController(IRepositoryManager repository)
         {
-            _logger = logger;
+            _repository = repository;
         }
+        
         [HttpGet]
-        public IEnumerable<string> Get()
+        public ActionResult<IEnumerable<string>> Get()
         {
-            _logger.LogInfo("Вот информационное сообщение от нашего контроллера значений.");//
-            _logger.LogDebug("Вот отладочное сообщение от нашего контроллера значений.");
-            _logger.LogWarn("Вот сообщение предупреждения от нашего контроллера значений.");
-            _logger.LogError("Вот сообщение об ошибке от нашего контроллера значений.");
+           // _repository.Company.AnyMethodFromCompanyRepository();
+           // _repository.Employee.AnyMethodFromEmployeeRepository();
             return new string[] { "value1", "value2" };
         }
-        
-        
     }
+        
+        
 }
+

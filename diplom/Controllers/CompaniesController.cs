ce7da33d679435e04669536d13ab0b54566dfb0d
Sergey Shatrov <2003df@maul.ru>
2022-11-03 00:01:23 +0300

    3 фулл готова

--- diplom/Controllers/CompaniesController.cs
+++ diplom/Controllers/CompaniesController.cs
@@ -27,24 +27,11 @@ namespace diplom.Controllers
         [HttpGet]
         public IActionResult GetCompanies()
         {
-            try
-            {
-                var companies = _repository.Company.GetAllCompanies(trackChanges: false);
-                var companiesDto = _mapper.Map<IEnumerable<CompanyDto>>(companies);
-
-                //var companiesDto = companies.Select(c => new CompanyDto
-               // {
-              //      Id = c.Id,
-               //     Name = c.Name,
-                //    FullAddress = string.Join(' ', c.Address, c.Country)
-              //  }).ToList();
-                return Ok(companiesDto);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"Something went wrong in the{nameof(GetCompanies)}action {ex}");
-                return StatusCode(500, "Internal server error");
-            }
+            var companies = _repository.Company.GetAllCompanies(trackChanges: false);
+            var companiesDto = _mapper.Map<IEnumerable<CompanyDto>>(companies);
+            
+            return Ok(companiesDto);
+            
         }
     }
 }

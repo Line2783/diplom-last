7491449bb0752578a7edaaeb3c3e32fcc8bcd759
Sergey Shatrov <2003df@maul.ru>
2022-11-03 00:23:05 +0300

    4,1

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Contracts;
 using Entities;
@@ -15,5 +16,7 @@ namespace Repository
 
         public IEnumerable<Company> GetAllCompanies(bool trackChanges) => 
             FindAll(trackChanges).OrderBy(c => c.Name).ToList();
+        public Company GetCompany(Guid companyId, bool trackChanges) => FindByCondition(c 
+            => c.Id.Equals(companyId), trackChanges) .SingleOrDefault();
     }
 }

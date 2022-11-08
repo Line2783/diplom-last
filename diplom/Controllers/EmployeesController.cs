ecafdebf7109a03c4af20ee255407e8e7a7199a5
Sergey Shatrov <2003df@maul.ru>
2022-11-09 00:05:57 +0300

    7 готова полностью без своих контроллеров!

--- diplom/Controllers/EmployeesController.cs
+++ diplom/Controllers/EmployeesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using AutoMapper;
 using Contracts;
 using Entities.DataTransferObjects;
@@ -28,15 +29,15 @@ namespace diplom.Controllers
         
         [HttpGet("{id}", Name = "GetEmployeeForCompany")] 
 
-        public IActionResult GetEmployeeForCompany(Guid companyId, Guid id)
+        public async Task<IActionResult> GetEmployeeForCompany(Guid companyId, Guid id)
         {
-            var company = _repository.Company.GetCompany(companyId, trackChanges: false);
+            var company = await _repository.Company.GetCompanyAsync(companyId, trackChanges: false);
             if (company == null)
             {
                 _logger.LogInfo($"Company with id: {companyId} doesn't exist in the database.");
                 return NotFound();
             }
-            var employeeDb = _repository.Employee.GetEmployee(companyId, id,
+            var employeeDb = await _repository.Employee.GetEmployeeAsync(companyId, id,
                 trackChanges:
                 false);
             if (employeeDb == null)
@@ -48,7 +49,7 @@ namespace diplom.Controllers
             return Ok(employee);
         }
         [HttpPost]
-        public IActionResult CreateEmployeeForCompany(Guid companyId, [FromBody]
+        public async Task<IActionResult> CreateEmployeeForCompany(Guid companyId, [FromBody]
             EmployeeForCreationDto employee)
         {
             if (employee == null)
@@ -61,7 +62,7 @@ namespace diplom.Controllers
                 _logger.LogError("Invalid model state for the EmployeeForCreationDto object");
                 return UnprocessableEntity(ModelState);
             }
-            var company = _repository.Company.GetCompany(companyId, trackChanges: false);
+            var company = await _repository.Company.GetCompanyAsync(companyId, trackChanges: false);
             if (company == null)
             {
                 _logger.LogInfo($"Company with id: {companyId} doesn't exist in the database.");
@@ -69,7 +70,7 @@ namespace diplom.Controllers
             }
             var employeeEntity = _mapper.Map<Employee>(employee);
             _repository.Employee.CreateEmployeeForCompany(companyId, employeeEntity);
-            _repository.Save();
+            await _repository.SaveAsync();
             var employeeToReturn = _mapper.Map<EmployeeDto>(employeeEntity);
             return CreatedAtRoute("GetEmployeeForCompany", new
             {
@@ -77,15 +78,15 @@ namespace diplom.Controllers
             }, employeeToReturn);
         }
         [HttpDelete("{id}")]
-        public IActionResult DeleteEmployeeForCompany(Guid companyId, Guid id)
+        public async Task<IActionResult> DeleteEmployeeForCompany(Guid companyId, Guid id)
         {
-            var company = _repository.Company.GetCompany(companyId, trackChanges: false);
+            var company = await _repository.Company.GetCompanyAsync(companyId, trackChanges: false);
             if (company == null)
             {
                 _logger.LogInfo($"Company with id: {companyId} doesn't exist in the database.");
                 return NotFound();
             }
-            var employeeForCompany = _repository.Employee.GetEmployee(companyId, id,
+            var employeeForCompany = await _repository.Employee.GetEmployeeAsync(companyId, id,
                 trackChanges: false);
             if (employeeForCompany == null)
             {
@@ -93,11 +94,11 @@ namespace diplom.Controllers
                 return NotFound();
             }
             _repository.Employee.DeleteEmployee(employeeForCompany);
-            _repository.Save();
+            await _repository.SaveAsync();
             return NoContent();
         }
         [HttpPut("{id}")]
-        public IActionResult UpdateEmployeeForCompany(Guid companyId, Guid id, [FromBody]
+        public async Task<IActionResult> UpdateEmployeeForCompany(Guid companyId, Guid id, [FromBody]
             EmployeeForUpdateDto employee)
         {
             if (employee == null)
@@ -110,13 +111,13 @@ namespace diplom.Controllers
                 _logger.LogError("Invalid model state for the EmployeeForUpdateDto object");
                 return UnprocessableEntity(ModelState);
             }
-            var company = _repository.Company.GetCompany(companyId, trackChanges: false);
+            var company = await _repository.Company.GetCompanyAsync(companyId, trackChanges: false);
             if (company == null)
             {
                 _logger.LogInfo($"Company with id: {companyId} doesn't exist in the database.");
                 return NotFound();
             }
-            var employeeEntity = _repository.Employee.GetEmployee(companyId, id,
+            var employeeEntity = await _repository.Employee.GetEmployeeAsync(companyId, id,
                 trackChanges:
                 true);
             if (employeeEntity == null)
@@ -125,12 +126,12 @@ namespace diplom.Controllers
                 return NotFound();
             }
             _mapper.Map(employee, employeeEntity);
-            _repository.Save();
+           await _repository.SaveAsync();
             return NoContent();
         }
         
         [HttpPatch("{id}")]
-        public IActionResult PartiallyUpdateEmployeeForCompany(Guid companyId, Guid id,
+        public async Task<IActionResult> PartiallyUpdateEmployeeForCompany(Guid companyId, Guid id,
             [FromBody] JsonPatchDocument<EmployeeForUpdateDto> patchDoc)
         {
             if (patchDoc == null)
@@ -138,13 +139,13 @@ namespace diplom.Controllers
                 _logger.LogError("patchDoc object sent from client is null.");
                 return BadRequest("patchDoc object is null");
             }
-            var company = _repository.Company.GetCompany(companyId, trackChanges: false);
+            var company = await _repository.Company.GetCompanyAsync(companyId, trackChanges: false);
             if (company == null)
             {
                 _logger.LogInfo($"Company with id: {companyId} doesn't exist in the database.");
                 return NotFound();
             }
-            var employeeEntity = _repository.Employee.GetEmployee(companyId, id,
+            var employeeEntity = await _repository.Employee.GetEmployeeAsync(companyId, id,
                 trackChanges:
                 true);
             if (employeeEntity == null)
@@ -163,7 +164,7 @@ namespace diplom.Controllers
             
             
             _mapper.Map(employeeToPatch, employeeEntity);
-            _repository.Save();
+           await _repository.SaveAsync();
             return NoContent();
         }
     }

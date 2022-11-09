3875d555d303ad9ed095fe886d710973fb27f8cd
youremywhore <95357313+youremywhore@users.noreply.github.com>
2022-11-09 10:18:31 +0300

    7.2.4

--- Contracts/IClientRepository.cs
+++ Contracts/IClientRepository.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Entities.Models;
 
 namespace Contracts
 {
     public interface IClientRepository
     {
-        IEnumerable<Client> GetAllClients(bool trackChanges);
-        Client GetClient(Guid clientId, bool trackChanges);
+        Task<IEnumerable<Client>> GetAllClientsAsync(bool trackChanges);
+        Task<Client> GetClientAsync(Guid clientId, bool trackChanges);
+        Task<IEnumerable<Client>> GetByIdsAsync(IEnumerable<Guid> ids, bool trackChanges); 
         void CreateClient(Client client);
-        IEnumerable<Client> GetByIds(IEnumerable<Guid> ids, bool trackChanges); 
         void DeleteClient(Client client);
 
 

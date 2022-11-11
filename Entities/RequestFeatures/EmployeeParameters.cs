f121634a116d3838ca68706f62987b480d374823
Sergey Shatrov <2003df@maul.ru>
2022-11-12 00:17:37 +0300

    9.1

--- Entities/RequestFeatures/EmployeeParameters.cs
+++ Entities/RequestFeatures/EmployeeParameters.cs
@@ -16,5 +16,7 @@
         public uint MinAge { get; set; }
         public uint MaxAge { get; set; } = int.MaxValue;
         public bool ValidAgeRange => MaxAge > MinAge;
+        public string SearchTerm { get; set; }
+
     }
 }
--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -7,6 +7,7 @@ using Entities;
 using Entities.Models;
 using Entities.RequestFeatures;
 using Microsoft.EntityFrameworkCore;
+using Repository.Extensions;
 
 namespace Repository
 {
@@ -17,14 +18,14 @@ namespace Repository
         {
         }
         public async Task<PagedList<Employee>> GetEmployeesAsync(Guid companyId,
-            EmployeeParameters employeeParameters, bool trackChanges)
+            EmployeeParameters employeeParameters,
+            bool trackChanges)
         {
-            var employees = await FindByCondition(e => e.CompanyId.Equals(companyId) &&
-                                                       (e.Age
-                                                           >= employeeParameters.MinAge && e.Age <= employeeParameters.MaxAge),
+            var employees = await FindByCondition(e => e.CompanyId.Equals(companyId),
                     trackChanges)
-                .OrderBy(e => e.Name)
-                .ToListAsync();
+                .FilterEmployees(employeeParameters.MinAge,
+                    employeeParameters.MaxAge).Search(employeeParameters.SearchTerm)
+                .OrderBy(e => e.Name).ToListAsync();
             return PagedList<Employee>
                 .ToPagedList(employees, employeeParameters.PageNumber,
                     employeeParameters.PageSize);

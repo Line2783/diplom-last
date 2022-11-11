94d834a330ca11866e2e86e2c40bf6bbf785442e
Sergey Shatrov <2003df@maul.ru>
2022-11-12 00:29:07 +0300

    9.2

--- Repository/Extensions/RepositoryEmployeeExtensions.cs
+++ Repository/Extensions/RepositoryEmployeeExtensions.cs
@@ -1,5 +1,11 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Entities.Models;
+using System.Linq.Dynamic.Core;
+using System.Reflection;
+using System.Text;
+using Repository.Extensions.Utility;
+
 
 namespace Repository.Extensions
 {
@@ -17,6 +23,19 @@ namespace Repository.Extensions
             var lowerCaseTerm = searchTerm.Trim().ToLower();
             return employees.Where(e => e.Name.ToLower().Contains(lowerCaseTerm));
             
+        }
+        public static IQueryable<Employee> Sort(this IQueryable<Employee> employees,
+            string orderByQueryString)
+        {
+            if (string.IsNullOrWhiteSpace(orderByQueryString))
+                return employees.OrderBy(e => e.Name);
+
+            var orderQuery =
+                OrderQueryBuilder.CreateOrderQuery<Employee>(orderByQueryString);
+
+            if (string.IsNullOrWhiteSpace(orderQuery))
+                return employees.OrderBy(e => e.Name);
+            return employees.OrderBy(orderQuery);
         }
     }
 

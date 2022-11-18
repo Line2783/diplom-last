773f8774d6be2155690c160c9d94551ef4d3f1f3
Sergey Shatrov <2003df@maul.ru>
2022-11-18 16:53:50 +0300

    модели отель и объявление

---
+++ Entities/Models/Advertisement.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models
+{
+    public class Advertisement
+    {
+        [Column("AdvertisementId")]
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "Advertisement name is a required field.")]
+        [MaxLength(60, ErrorMessage = "Maximum length for the Name is 60 characters.")] 
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Advertisement city is a required field.")]
+        [MaxLength(60, ErrorMessage = "Maximum length for rhe Address is 60 character")]
+        public string City { get; set; }
+        [Required(ErrorMessage = "Advertisement address is a required field.")]
+        [MaxLength(60, ErrorMessage = "Maximum length for rhe Address is 60 character")]
+        public string Address { get; set; }
+        [Required(ErrorMessage = "Advertisement description is a required field.")]
+        [MaxLength(60, ErrorMessage = "Maximum length for rhe Address is 60 character")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Advertisement number is a required field.")]
+        [MaxLength(60, ErrorMessage = "Maximum length for rhe Address is 60 character")]
+        public string Number { get; set; }
+        [Required(ErrorMessage = "Advertisement cat is a required field.")]
+        public Boolean Cat { get; set; }
+        [Required(ErrorMessage = "Advertisement dog is a required field.")]
+        public Boolean Dog { get; set; }
+        [Required(ErrorMessage = "Advertisement rodent is a required field.")]
+        public Boolean Rodent { get; set; }
+        [Required(ErrorMessage = "Advertisement other is a required field.")]
+        public Boolean Other { get; set; }
+        [ForeignKey(nameof(Company))]
+        public Guid HotelId { get; set; }
+        public Hotel Hotel { get; set; }
+    }
+}
